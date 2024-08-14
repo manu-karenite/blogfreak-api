1de63ffe0bfb6ca604e354ba940a443eb995f467
Manavesh Narendra <manavesh.narendra@gmail.com>
2024-08-14 16:10:57 +0530

    Made Schema Generation Dynamic - Example was overriding.

--- src/main/java/com/blogfreak/blog_freak_api/oas/schema/success/SuccessListOfAllBloggers.java
+++ src/main/java/com/blogfreak/blog_freak_api/oas/schema/success/SuccessListOfAllBloggers.java
@@ -3,18 +3,16 @@ package com.blogfreak.blog_freak_api.oas.schema.success;
 import com.blogfreak.blog_freak_api.entity.Blogger;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Schema(
-        example =
-                "{\"timestamp\":\"2024-07-31T15:58:00.523+00:00\",\"statusCodeMessage\":\"OK\",\"statusCode\":200,\"data\":[{\"id\":\"1722440932935\",\"firstName\":\"Manavesh\",\"lastName\":\"Narendra\",\"emailId\":\"manavesh.narendra@gmail.com\",\"gender\":\"male\",\"registeredAt\":\"2024-07-30T18:30:00.000+00:00\"}]}")
+@Schema
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SuccessListOfAllBloggers extends BaseSuccess {
     private List<Blogger> listOfAllBloggers;
-
-    public List<Blogger> getListOfAllBloggers() {
-        return listOfAllBloggers;
-    }
-
-    public void setListOfAllBloggers(List<Blogger> listOfAllBloggers) {
-        this.listOfAllBloggers = listOfAllBloggers;
-    }
 }
